b021de3f6776ce14023b45ae3e945daab13aeec3
Jugachi <contact@jugachi.com>
2025-11-15 17:33:36 +0100

    Games can now be added via Twitch Category IDs

--- Client/src/composables/SelectGameDialog.vue
+++ Client/src/composables/SelectGameDialog.vue
@@ -102,13 +102,25 @@
 						:disabled="isLoading"
 						:loading="isLoading"
 						:error-messages="searchErrorMessages"
-						label="Search"
+						label="Search by Name or ID"
 						append-inner-icon="mdi-magnify"
-						placeholder="Just Chatting"
+						placeholder="Just Chatting or ID=509658"
 						required
 						variant="solo-filled"
 						hide-details="auto"
 					/>
+				</v-row>
+				<v-row class="mt-2">
+					<v-col class="text-caption">
+						To find the correct Twitch Category ID, you can use this page:
+						<a
+							href="https://aquillium.com/get-twitch-category-id/"
+							target="_blank"
+							rel="noopener"
+						>
+							aquillium.com/get-twitch-category-id
+						</a>
+					</v-col>
 				</v-row>
 			</v-card-text>
 
