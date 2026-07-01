e17cececc91bfa98e389d52a7f74e0ef008f4db3
FreezyLemon <contact@freezylemon.com>
2026-07-01 19:21:21 +0200

    Server: create basic eslint config

---
+++ Server/eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        extends: ["js/recommended"],
+        plugins: {
+            js
+        },
+        languageOptions: {
+            // see https://node.green/ for supported ES versions
+            ecmaVersion: 14,
+            sourceType: "module"
+        },
+        // Place for our own rules on top of js/recommended
+        rules: {}
+    }
+]);
